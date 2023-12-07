3aeefe63f8a862913263d1271d8be3d96d10e886
ThanphisitW <kimgongbu2003@gmail.com>
2023-12-07 17:53:42 +0700

    Update character_guessing_game.py

--- character_guessing_game.py
+++ character_guessing_game.py
@@ -3,15 +3,15 @@ import openai
 import json
 import pandas as pd
 
-# user_api_key = st.sidebar.text_input("OpenAPI API key", type="password")
+user_api_key = st.sidebar.text_input("OpenAPI API key", type="password")
 
-# client = openai.OpenAI(api_key=user_api_key)
-# prompt = """Act as an Akinator. You will received a clue from player and your job is to guess the character or person that player think of.
-# You will first ask the player "Is your character a real person?", then you will start to guess the character based on the player's answer.
-# """
+client = openai.OpenAI(api_key=user_api_key)
+prompt = """Act as an Akinator. You will received a clue from player and your job is to guess the character or person that player think of.
+You will first ask the player "Is your character a real person?", then you will start to guess the character based on the player's answer.
+"""
 
-# st.title("Character Guessing Game")
-# st.markdown("Please remind that the AI model is based on the information till 2021.")
+st.title("Character Guessing Game")
+st.markdown("Please remind that the AI model is based on the information till 2021.")
 
 # user_input = st.text_input("Please answer yes or no", 'yes')
 # submit_button = st.button("Submit")
@@ -36,30 +36,21 @@ import pandas as pd
 #     # Add the AI's response to the conversation history
 #     messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
 
-user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
+asked_real_person = st.session_state.get("asked_real_person", False)
 
-client = openai.OpenAI(api_key=user_api_key)
-prompt = """Act as an Akinator. You will receive a clue from the player, and your job is to guess the character or person that the player is thinking of.
-You will first ask the player "Is your character a real person?", then you will start to guess the character based on the player's answer.
-"""
-
-st.title("Character Guessing Game")
-st.markdown("Please remind that the AI model is based on the information till 2021.")
-
-messages_so_far = [
-    {"role": "system", "content": prompt},
-]
-
-if st.session_state.get("asked_real_person", False) is False:
+if not asked_real_person:
     # Ask the initial question about whether the character is a real person
     user_input = st.radio("Is your character a real person?", ["Yes", "No"])
-    messages_so_far.append({"role": "user", "content": f"{user_input.lower()}"})
     st.session_state.asked_real_person = True  # Set a flag to indicate that the question has been asked
 else:
     # Continue with the guessing based on the player's answers
     user_input = st.text_input("Please answer yes or no", "yes")
 
-messages_so_far.append({"role": "user", "content": user_input})
+messages_so_far = [
+    {"role": "system", "content": prompt},
+    {"role": "user", "content": user_input},
+]
+
 response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=messages_so_far,
@@ -72,5 +63,6 @@ ai_response_text = response.choices[0].message.content
 st.markdown("**AI response:**")
 st.write(ai_response_text)
 
-# Add the AI's response to the conversation history
-messages_so_far.append({"role": "assistant", "content": ai_response_text})
+# Clear session state if the user wants to play again
+if st.button("Play Again"):
+    st.session_state.asked_real_person = False

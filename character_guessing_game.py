aa234e2a170ebfbd56b286fb9157ed2898037d48
ThanphisitW <kimgongbu2003@gmail.com>
2023-12-07 17:33:36 +0700

    Update character_guessing_game.py

--- character_guessing_game.py
+++ character_guessing_game.py
@@ -11,10 +11,54 @@ After the player answer whether their character is a real person or fictional ch
 """
 
 st.title("Character Guessing Game")
-st.markdown("Please remind that the AI model is based on the information till 2021.")
 st.header("Is your character a real person or fictional character?")
+st.markdown("Please remind that the AI model is based on the information till 2021.")
 user_input = st.text_input("Please answer yes or no", 'yes')
 submit_button = st.button("Submit")
+
+messages_so_far = [
+    {"role": "system", "content": prompt},
+]
+
+if submit_button:
+    if user_input.lower() == 'yes':
+        messages_so_far.append({'role': 'user', 'content': user_input})
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages_so_far
+        )
+        # Extract the message content from the AI's response
+        ai_response_text = response.choices[0].message.content
+
+        # Print the AI's response
+        st.markdown('**AI response:**')
+        st.write(ai_response_text)
+
+        # Add the AI's response to the conversation history
+        messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
+
+    elif user_input.lower() == 'no':
+        messages_so_far.append({'role': 'user', 'content': user_input})
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages_so_far
+        )
+        # Extract the message content from the AI's response
+        ai_response_text = response.choices[0].message.content
+
+        # Print the AI's response
+        st.markdown('**AI response:**')
+        st.write(ai_response_text)
+
+        # Add the AI's response to the conversation history
+        messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
+
+
+
+
+
+
+
 
 
 # if submit_button:
@@ -48,27 +92,3 @@ submit_button = st.button("Submit")
 #         # Print the AI's response
 #         st.markdown('**AI response:**')
 #         st.write(ai_response_text)
-
-messages_so_far = [
-    {"role": "system", "content": prompt},
-]
-
-if submit_button:
-    # Add the user's input to the conversation history
-    messages_so_far.append({'role': 'user', 'content': user_input})
-
-    # Generate a new message from the AI
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages_so_far
-    )
-
-    # Extract the message content from the AI's response
-    ai_response_text = response.choices[0].message.content
-
-    # Print the AI's response
-    st.markdown('**AI response:**')
-    st.write(ai_response_text)
-
-    # Add the AI's response to the conversation history
-    messages_so_far.append({'role': 'assistant', 'content': ai_response_text})

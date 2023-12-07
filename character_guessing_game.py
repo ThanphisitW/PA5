f0c69d53dbdbf753f1853f472dabff7592365369
ThanphisitW <kimgongbu2003@gmail.com>
2023-12-07 17:50:52 +0700

    Update character_guessing_game.py

--- character_guessing_game.py
+++ character_guessing_game.py
@@ -3,35 +3,74 @@ import openai
 import json
 import pandas as pd
 
-user_api_key = st.sidebar.text_input("OpenAPI API key", type="password")
+# user_api_key = st.sidebar.text_input("OpenAPI API key", type="password")
+
+# client = openai.OpenAI(api_key=user_api_key)
+# prompt = """Act as an Akinator. You will received a clue from player and your job is to guess the character or person that player think of.
+# You will first ask the player "Is your character a real person?", then you will start to guess the character based on the player's answer.
+# """
+
+# st.title("Character Guessing Game")
+# st.markdown("Please remind that the AI model is based on the information till 2021.")
+
+# user_input = st.text_input("Please answer yes or no", 'yes')
+# submit_button = st.button("Submit")
+
+# messages_so_far = [
+#     {"role": "system", "content": prompt},
+# ]
+
+# if submit_button:
+#     messages_so_far.append({'role': 'user', 'content': user_input})
+#     response = client.chat.completions.create(
+#         model="gpt-3.5-turbo",
+#         messages=messages_so_far
+#     )
+#     # Extract the message content from the AI's response
+#     ai_response_text = response.choices[0].message.content
+
+#     # Print the AI's response
+#     st.markdown('**AI response:**')
+#     st.write(ai_response_text)
+
+#     # Add the AI's response to the conversation history
+#     messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
+
+user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
-prompt = """Act as an Akinator. You will received a clue from player and your job is to guess the character or person that player think of.
+prompt = """Act as an Akinator. You will receive a clue from the player, and your job is to guess the character or person that the player is thinking of.
 You will first ask the player "Is your character a real person?", then you will start to guess the character based on the player's answer.
 """
 
 st.title("Character Guessing Game")
 st.markdown("Please remind that the AI model is based on the information till 2021.")
 
-user_input = st.text_input("Please answer yes or no", 'yes')
-submit_button = st.button("Submit")
-
 messages_so_far = [
     {"role": "system", "content": prompt},
 ]
 
-if submit_button:
-    messages_so_far.append({'role': 'user', 'content': user_input})
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages_so_far
-    )
-    # Extract the message content from the AI's response
-    ai_response_text = response.choices[0].message.content
+if st.session_state.get("asked_real_person", False) is False:
+    # Ask the initial question about whether the character is a real person
+    user_input = st.radio("Is your character a real person?", ["Yes", "No"])
+    messages_so_far.append({"role": "user", "content": f"{user_input.lower()}"})
+    st.session_state.asked_real_person = True  # Set a flag to indicate that the question has been asked
+else:
+    # Continue with the guessing based on the player's answers
+    user_input = st.text_input("Please answer yes or no", "yes")
 
-    # Print the AI's response
-    st.markdown('**AI response:**')
-    st.write(ai_response_text)
+messages_so_far.append({"role": "user", "content": user_input})
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=messages_so_far,
+)
 
-    # Add the AI's response to the conversation history
-    messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
+# Extract the message content from the AI's response
+ai_response_text = response.choices[0].message.content
+
+# Print the AI's response
+st.markdown("**AI response:**")
+st.write(ai_response_text)
+
+# Add the AI's response to the conversation history
+messages_so_far.append({"role": "assistant", "content": ai_response_text})

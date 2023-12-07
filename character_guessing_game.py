1e80717a6417eb363af85e0b513d36c9ffcb47b9
ThanphisitW <kimgongbu2003@gmail.com>
2023-12-07 16:59:25 +0700

    Update character_guessing_game.py

--- character_guessing_game.py
+++ character_guessing_game.py
@@ -6,32 +6,32 @@ import pandas as pd
 user_api_key = st.sidebar.text_input("OpenAPI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
-prompt = """Act as a Akinator. You will received a clue from player and your job is to guess the character or person that player think of.
+prompt = """Act as an Akinator. You will received a clue from player and your job is to guess the character or person that player think of.
 You will first ask the player if their character is a real person or fictional character. Then, you will guess based on player's answer.
 """
 
 st.title("Character Guessing Game")
-# Initial message from chatGPT
-st.write("Is your character a real person?")
-description = st.text_input("Please give a short description about your character", 'He is blonde and has spiky hair')
+st.markdown("Please remind that the AI model is based on the information till 2021.")
+user_input = st.text_input("Please answer yes or no", 'yes')
 submit_button = st.button("Submit")
 
-if submit_button:
-    message = [
-        {"role": "system", "content": "Is your character a real person?"},
-        {'role': 'user', 'content': description}
+if st.button('Submit'):
+    messages_so_far = [
+        {"role": "system", "content": prompt},
+        {'role': 'user', 'content': user_input},
     ]
-    for i in range(40):  # Limit to 40 guesses
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=message
-        )
-        suggestion_dictionary = response.choices[0].message.content
-        print(suggestion_dictionary)  # Print out the data you're trying to parse
-        if suggestion_dictionary:  # Check if the response is not empty
-            sd = json.loads(suggestion_dictionary)
-            st.write(f'AI Guess {i+1}: {sd["content"]}')
-        else:
-            st.write("The AI didn't return any guesses.")
-        user_response = st.text_input("Please respond with yes or no, or give a hint if the question is repetitive.")
-        message.append({'role': 'user', 'content': user_response})
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages_so_far
+    )
+    # Show the response from the AI in a box
+    st.markdown('**AI response:**')
+    suggestion_dictionary = response.choices[0].message.content
+
+
+    sd = json.loads(suggestion_dictionary)
+
+    print (sd)
+    suggestion_df = pd.DataFrame.from_dict(sd)
+    print(suggestion_df)
+    st.table(suggestion_df)

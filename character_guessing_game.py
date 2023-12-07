af3ed513c3c1083d23ff8a72891828470188b301
ThanphisitW <kimgongbu2003@gmail.com>
2023-12-07 17:47:05 +0700

    Update character_guessing_game.py

--- character_guessing_game.py
+++ character_guessing_game.py
@@ -21,38 +21,20 @@ messages_so_far = [
 ]
 
 if submit_button:
-    if user_input.lower() == 'yes':
-        messages_so_far.append({'role': 'user', 'content': user_input})
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages_so_far
-        )
-        # Extract the message content from the AI's response
-        ai_response_text = response.choices[0].message.content
+    messages_so_far.append({'role': 'user', 'content': user_input})
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages_so_far
+    )
+    # Extract the message content from the AI's response
+    ai_response_text = response.choices[0].message.content
 
-        # Print the AI's response
-        st.markdown('**AI response:**')
-        st.write(ai_response_text)
+    # Print the AI's response
+    st.markdown('**AI response:**')
+    st.write(ai_response_text)
 
-        # Add the AI's response to the conversation history
-        messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
-
-    elif user_input.lower() == 'no':
-        messages_so_far.append({'role': 'user', 'content': user_input})
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages_so_far
-        )
-        # Extract the message content from the AI's response
-        ai_response_text = response.choices[0].message.content
-
-        # Print the AI's response
-        st.markdown('**AI response:**')
-        st.write(ai_response_text)
-
-        # Add the AI's response to the conversation history
-        messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
-
+    # Add the AI's response to the conversation history
+    messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
 
 
 

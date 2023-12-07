f04a3f4d8df02a92e885f61229cf61a84ca407e8
ThanphisitW <kimgongbu2003@gmail.com>
2023-12-07 17:43:47 +0700

    Update character_guessing_game.py

--- character_guessing_game.py
+++ character_guessing_game.py
@@ -11,9 +11,10 @@ After the player answer whether their character is a real person or fictional ch
 """
 
 st.title("Character Guessing Game")
-st.header("Is your character a real person or fictional character?")
+st.header("Is your character a real person?")
 st.markdown("Please remind that the AI model is based on the information till 2021.")
-user_input = st.text_input("Please answer yes or no", 'yes')
+
+user_input = st.radio("Please answer", ["Yes", "No"])
 submit_button = st.button("Submit")
 
 messages_so_far = [
@@ -21,37 +22,62 @@ messages_so_far = [
 ]
 
 if submit_button:
-    if user_input.lower() == 'yes':
-        messages_so_far.append({'role': 'user', 'content': user_input})
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages_so_far
-        )
-        # Extract the message content from the AI's response
-        ai_response_text = response.choices[0].message.content
+    messages_so_far.append({"role": "user", "content": f"{user_input.lower()}"})
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages_so_far,
+    )
 
-        # Print the AI's response
-        st.markdown('**AI response:**')
-        st.write(ai_response_text)
+    # Extract the message content from the AI's response
+    ai_response_text = response.choices[0].message.content
 
-        # Add the AI's response to the conversation history
-        messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
+    # Print the AI's response
+    st.markdown("**AI response:**")
+    st.write(ai_response_text)
 
-    elif user_input.lower() == 'no':
-        messages_so_far.append({'role': 'user', 'content': user_input})
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=messages_so_far
-        )
-        # Extract the message content from the AI's response
-        ai_response_text = response.choices[0].message.content
+    # Add the AI's response to the conversation history
+    messages_so_far.append({"role": "assistant", "content": ai_response_text})
 
-        # Print the AI's response
-        st.markdown('**AI response:**')
-        st.write(ai_response_text)
 
-        # Add the AI's response to the conversation history
-        messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
+# user_input = st.text_input("Please answer yes or no", 'yes')
+# submit_button = st.button("Submit")
+
+# messages_so_far = [
+#     {"role": "system", "content": prompt},
+# ]
+
+# if submit_button:
+#     if user_input.lower() == 'yes':
+#         messages_so_far.append({'role': 'user', 'content': user_input})
+#         response = client.chat.completions.create(
+#             model="gpt-3.5-turbo",
+#             messages=messages_so_far
+#         )
+#         # Extract the message content from the AI's response
+#         ai_response_text = response.choices[0].message.content
+
+#         # Print the AI's response
+#         st.markdown('**AI response:**')
+#         st.write(ai_response_text)
+
+#         # Add the AI's response to the conversation history
+#         messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
+
+#     elif user_input.lower() == 'no':
+#         messages_so_far.append({'role': 'user', 'content': user_input})
+#         response = client.chat.completions.create(
+#             model="gpt-3.5-turbo",
+#             messages=messages_so_far
+#         )
+#         # Extract the message content from the AI's response
+#         ai_response_text = response.choices[0].message.content
+
+#         # Print the AI's response
+#         st.markdown('**AI response:**')
+#         st.write(ai_response_text)
+
+#         # Add the AI's response to the conversation history
+#         messages_so_far.append({'role': 'assistant', 'content': ai_response_text})
 
 
 

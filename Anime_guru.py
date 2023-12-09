3475208a3112e532ec50599ae04f4dc89699a51d
ThanphisitW <kimgongbu2003@gmail.com>
2023-12-10 04:52:28 +0700

    Update Anime_guru.py

--- Anime_guru.py
+++ Anime_guru.py
@@ -19,12 +19,12 @@ with open("streamlit.css") as f:
                         <a id="logo" href="#home"><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/logo.PNG?raw=true" alt="Logo Image"></a>
                     </div>      
                     <nav class="main-nav">                
-                        <a href="#home">Home</a>
-                        <a href="#tvShows">TV Shows</a>
-                        <a href="#movies">Movies</a>
-                        <a href="#originals">Originals</a>
-                        <a href="#">Recently Added</a>
-                        <a target="_blank" href="https://codepen.io/cb2307/full/NzaOrm">Portfolio</a>        
+                        <p>Home</p>
+                        <p>TV Shows</p>
+                        <p>Movies</p>
+                        <p>Originals</p>
+                        <p>Recently Added</p>
+                        <p>Portfolio</p>        
                     </nav>
                     <nav class="sub-nav">
                         <a href="#"><i class="fas fa-search sub-nav-logo"></i></a>

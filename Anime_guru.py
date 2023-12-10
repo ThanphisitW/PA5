08d5f1407b06df6555e646e36cdce482fc3ea30b
ThanphisitW <kimgongbu2003@gmail.com>
2023-12-10 22:43:54 +0700

    Update Anime_guru.py

--- Anime_guru.py
+++ Anime_guru.py
@@ -169,8 +169,8 @@ st.markdown("""
                         <p>Email : 6640105922@student.chula.ac.th</p>
                     </div>
                     <div>
-                        <a href="https://github.com/ThanphisitW/PA5"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub logo">Watch the code on GitHub</a>
-                        <a href="https://www.instagram.com/dpvmdla/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" alt="Instagram logo">Follow me on Instagram</a>
+                        <a href="https://github.com/ThanphisitW/PA5"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub logo"></a>
+                        <a href="https://www.instagram.com/dpvmdla/"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png" alt="Instagram logo"></a>
                     </div>
                 </div>
             </div>

19502506c33497f183c9903d63fe018018a68a9d
ThanphisitW <kimgongbu2003@gmail.com>
2023-12-10 22:32:54 +0700

    Update Anime_guru.py

--- Anime_guru.py
+++ Anime_guru.py
@@ -155,7 +155,7 @@ st.markdown("""
             }
             </style>
             <div class="footer">
-                <div>
+                <div style="display: flex; justify-content: flex-start;">
                     <div>
                         <h4>Contact</h4>
                         <p>Phone : +66 95 956 5752</p>
